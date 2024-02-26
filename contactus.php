6b49a8a32fd7c5845cc717b794fc13f32119623b
AbabilDH <161285907+AbabilDH@users.noreply.github.com>
2024-02-26 12:03:50 +0530

    Add files via upload

---
+++ contactus.php
@@ -0,0 +1,4 @@
+<p>Ibrahim saheb street,<br />
+Rashad nagar, APJ Abdul kalam,<br />
+Nagawara Main road<br />
+Bangalore north 560045</p>
---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>I Love You</h1>
+    <button>yes</button>
+    <button>No</button><hr />
+    <a href="./Aboutus.php">About us</a><br />
+    <a href="contactus.php"> Contact us</a>
+</body>
+</html>
